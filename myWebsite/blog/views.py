43537234e6817f4bc6e6ce67537b3d699c252e79
sarah <sarahmutio315@gmail.com>
2024-01-18 21:50:56 +0200

    Added docstrings to views

--- myWebsite/blog/views.py
+++ myWebsite/blog/views.py
@@ -6,11 +6,33 @@ from .models import Post
 # Create your views here.
 
 class PostList(generic.ListView):
-    # Returns all posts that are with status 1 (published) abd order from the latest one.
+    """Sorts and creates a list of posts from the newest to the oldest post.
+
+        :param BaseManager queryset: Calls the Post class and filters the posts
+                                     by status 1 (published) then sort it by date
+                                     created from the newest to oldest
+        :param Literal template_name: The name of the template the content will be 
+                                      rendered on
+    """
 
     queryset = Post.objects.filter(status=1).order_by('-created_at')
     template_name = 'index.html'
 
 class PostDetail(generic.DetailView):
+    """Renders the content from the post onto the webpage.
+    
+        :param Post model: Calls the Post class from the models.py
+        :param Literal template_name: The name of the template the content will be 
+                                      rendered on
+    """
+
     model = Post
     template_name = 'post_detail.html'
+
+"""
+    Reference:
+    ------------------
+        * Wanted to be add more information to the blog.
+        Source: https://medium.com/geekculture/create-a-blog-with-django-60f529f1d8b6
+                https://sentry.io/answers/slug-in-django/#:~:text=Slug%20is%20a%20term%20from,in%20a%20human%2Dfriendly%20form.
+"""
