76c720bb47af1795c2c79305c3470fd9c8d6d9e8
sarah <sarahmutio315@gmail.com>
2024-01-18 16:30:44 +0200

    Added docstring to functions

--- myWebsite/account/views.py
+++ myWebsite/account/views.py
@@ -32,6 +32,21 @@ from django.contrib.auth.forms import AuthenticationForm
 
 
 def register(request):
+    """Generates the registration form by calling the UserRegistrationForm class from forms.py
+       If the user is logged in, they will be redirected to the home page. If the user is not logged 
+       in and does not have an account they will be redirected to the register webpage to create an 
+       account, once they filled in the fields the user will be redirected to the home page. If a problem
+       occurs when the user tries to create an account, an error message will be displayed when the 
+       registration web page will be reload. The register.html page will be user to render the form.
+      
+       :param request: Used to procress any HttpResponse requests
+       :param user: Used to call the save function from the forms.py to save the user's input
+       :param error: Returns the error if an issue arises
+       :return: HttpResponse calls the django.template.loader.render_to_string function with parsed arguements
+                to render the form on the register.html page
+       :rtype: HttpResponse
+    """
+    
     if request.user.is_authenticated:
         return redirect("/home/")
 
@@ -65,6 +80,14 @@ def register(request):
 
 @login_required
 def custom_logout(request):
+    """When the user logs out of their account, a message will display to indicate that
+       they have successfully logged out.
+       
+       :param request: Used to procress any HttpResponse requests
+       :return: Redirects the user to the home web page
+       :rtype: HttpResponseRedirect
+       """
+    
     logout(request)
     messages.info(request, "Logged out successfully!")
     return redirect("/home/")
@@ -82,6 +105,24 @@ def custom_logout(request):
 '''
 
 def custom_login(request):
+    """Logs the user into the account. The user is required to fill all the field,
+       the credentials entered will be checked in the User model. 
+       - If they match they user will be redirected to the home webpage and a message 
+         will display that they have successfully logged into their account. 
+       - If the credentials are invalid, an error 
+         message will be displayed for the user to see, the form will be reloaded. 
+        - If the 
+          user is logged in and try to log in, they will be redirected to the home web page.
+       
+       :param request: Used to procress any HttpResponse requests
+       :param form: Calls the AuthenticationForm class
+       :param user: Calls the authenticate function to check the credentials of username and password
+       :param error: Returns the error if an issue arises
+       :return: HttpResponse calls the django.template.loader.render_to_string function with parsed arguements
+                to render the form on the login.html page
+       :rtype: HttpResponse
+       """
+    
     if request.user.is_authenticated:
         return redirect("/home/")
     
