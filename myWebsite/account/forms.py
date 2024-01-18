0f371d8ee310d2a328db25cdd20e3573a80cda62
sarah <sarahmutio315@gmail.com>
2024-01-18 15:48:13 +0200

    Added docstrings to the classes and function

--- myWebsite/account/forms.py
+++ myWebsite/account/forms.py
@@ -2,37 +2,44 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import get_user_model
 
-# Explanation of Class:
-# _______________________________
-'''
-    - In this class a user creation form is created with custom register form.
-    - Using UserCreationForm, we can add the first name, username, and password to
-      the form.
-    - The Meta class generates the textboxes and fields for the first name, username, email,
-      password.
-        - The get_user_model will return the currently active user model by 
-          either a custom model specified in AUTH_USER_MODEL or use the default built-in User.
-    - The save method saves the input to the database under the User model.
-
-'''
-# ________________________________
-# End of Explanation
-
 class UserRegistrationForm(UserCreationForm):
+  """The class creates a custom user registration form. The form adds first name, passwords, 
+     and email fields. The information, entered into the fields will be saved under the User model.
 
-    email = forms.EmailField(help_text='Enter a valid email address.', required=True)
+     :param EmailField email: Adds the email field to the form, the help text that appears to help
+                              the user user, it is required to enter a username
+  """
 
-    class Meta:
-        model = get_user_model()
-        fields = ['first_name', 'username', 'password1', 'password2']
+  email = forms.EmailField(help_text='Enter a valid email address.', required=True)
 
-    def save(self, commit = True):
-        user = super(UserRegistrationForm, self).save(commit=False)
-        user.email = self.cleaned_data['email']
-        
-        if commit:
-            user.save()
-        return user
+  class Meta:
+      """The class generates the addition fields (first name, username, password1, and password2)
+         the get_user_model returns the currently active user model by either a custom model 
+         specified in AUTH_USER_MODEL or use the default built-in User
+         
+         :param AbstractBaseUser model: Returns the user model that is active in this project
+         :param list[str] fields: List of fields that will be created
+         """
+      
+      model = get_user_model()
+      fields = ['first_name', 'username', 'password1', 'password2']
+
+  def save(self, commit = True):
+      """Saves the user's input and adds them to the database under the User model. Returns the
+         input to user.
+
+         :param bool commit: Commits the data to the User model
+         :param user: The user's input from the UserRegistrationForm is then saved
+         :return: user input is saved to User model
+         :rtype: Any
+      """
+
+      user = super(UserRegistrationForm, self).save(commit=False)
+      user.email = self.cleaned_data['email']
+      
+      if commit:
+          user.save()
+      return user
 
 
 # References:
