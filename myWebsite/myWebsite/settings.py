eb36b25139a239de18cec23d54b019e9ea5d2bdc
sarah <sarahmutio315@gmail.com>
2024-01-30 17:37:36 +0200

    Updates settings and requirements

--- myWebsite/myWebsite/settings.py
+++ myWebsite/myWebsite/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 
+import os
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&nv5!8ln7mdxww45a2r+z4#t!udsl*6*sz$%b(3wfh7b8_j6%0'
+SECRET_KEY =''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -38,6 +40,7 @@ INSTALLED_APPS = [
     'account',
     'library',
     'crispy_forms',
+    'environ',
     'crispy_bootstrap5',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -122,6 +125,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = [
+    os.path.join(BASE_DIR, "static")
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
